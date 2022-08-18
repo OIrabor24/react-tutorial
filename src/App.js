ee0fde11d7815aa32d5e83b16060329ecffe7bbe
Osahon Irabor <osahonirabor@hotmail.com>
2022-08-17 18:38:37 -0700

    react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
+import Table from "./table"
+import Form from "./Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    state = {
+        characters: [],
+    };
 
-export default App;
+    removeCharacter = (index) => {
+        const { characters } = this.state 
+
+        this.setState({
+            characters: characters.filter((characters, i) => {
+                return i !== index
+            }),
+        })
+    }
+
+    handelSubmit = (character) => {
+        this.setState({ characters: [...this.state.characters, character]})
+    }
+
+    render() {
+    const { characters } = this.state 
+
+      return (
+        <div className="container">
+          <Table characterData={characters} removeCharacter={this.removeCharacter} />
+          <Form handelSubmit={this.handelSubmit}/>
+        </div>
+      )
+    }
+  }
+
+
+export default App
